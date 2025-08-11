adc0ea52acd48ff518e14cfd39589d214200690e
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2025-08-11 11:39:44 +0100

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+school = "codingal"
+print(school)
+
+# user input
+name = input("enter your name: ")
+
+print(f"\nHello {name}\nwelcome to {school}\nyou would be learning python programming")
